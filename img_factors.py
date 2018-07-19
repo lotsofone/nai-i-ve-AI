ddb1a58f1418651a82e0dd3328c6052ff2ba41d5
lotsofone <552694725@qq.com>
2018-07-19 16:38:03 +0800

    hist stats

--- img_factors.py
+++ img_factors.py
@@ -9,12 +9,64 @@ import cv2
 import math
 import scipy.signal
 
-def loadimage(path):
-    image = PIL.Image.open(path)
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-    return image;
 
+def histogramGray(im):
+    gcount = numpy.zeros(256)
+    def gcc(v):
+        gcount[v]+=1
+        return 0
+
+    list(map(gcc, im.flatten()))
+    gcount/=len(im.flatten())
+    return gcount
+
+def histStats(gg):
+    props = {}
+    avg=0
+    for i in range(256):
+        avg+=i*gg[i]
+    props['avg']=avg
+    var=0
+    for i in range(256):
+        var+=(i-avg)*(i-avg)*gg[i]
+    props['var']=var
+    max=255
+    while max>=0:
+        if(gg[max]>0):
+            break
+        max-=1
+
+    min=0
+    while min<256:
+        if(gg[min]>0):
+            break
+        min+=1
+    props['max']=max
+    props['min']=min
+    skewness=0
+    for i in range(256):
+        skewness+=(i-avg)*(i-avg)*(i-avg)*gg[i]
+    skewness/=var
+    props['skewness']=skewness
+    kurtosis=0
+    for i in range(256):
+        kurtosis+=(i-avg)*(i-avg)*(i-avg)*(i-avg)*gg[i]
+    kurtosis/=var
+    kurtosis/=var
+    kurtosis-=3
+    props['kurtosis']=kurtosis
+    energy=0
+    for i in range(256):
+        energy+=gg[i]*gg[i]
+    props['energy']=energy
+    entropy = 0
+    for i in range(256):
+        if(gg[i]>0):
+            entropy+=gg[i]+math.log2(gg[i])
+    props['entropy']=entropy
+
+    return props
+"""
 image = loadimage("download\\829.jpg")
 im=numpy.array(image)
 #im=im.astype(numpy.float32)/255
@@ -53,3 +105,4 @@ matplotlib.pyplot.plot(numpy.arange(4), bcount, color="#0000FF")
 matplotlib.pyplot.subplot(2,1,2)
 matplotlib.pyplot.hist(gray.flatten(), 255, color="#888888")
 matplotlib.pyplot.show()
+"""
