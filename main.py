771d2ad85e7097975915b5a4ba4215b25c0904ff
honmameiko22 <honmameiko22@hotmail.com>
2018-07-23 16:23:27 +0800

    change
    change

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import glcm_lbp
 import csv
 import os
 import pandas
-
+import pickle
 def writetocsv(path, data):
 	#newline=""是为了防止输出空行
   with open(path,"a+", newline="") as csvfile: 
@@ -24,23 +24,29 @@ def pic(path):
 	directory = os.listdir(cwd)
 	
 	for dir in directory:
-		subpath = os.path.join(path, dir)
-		myfeatures=[]
-		print(subpath)
-		cnt=0
-		if os.path.isdir(subpath):
-			for subdir in os.listdir(subpath):
-				cnt+=1
-				print(cnt)
-				subsubpath=os.path.join(subpath, subdir)
-				#load image
-				img = numpy.array(base.loadimage(subsubpath))
-				print(subsubpath)
-				img = (skimage.color.rgb2gray(img)*255).astype(numpy.uint8)
-		        #features
-				myfeatures.append(base.feature(img))
-			data=pandas.DataFrame(myfeatures)
-			data.to_csv("C:\\Users\\asus-pc\\Documents\\GitHub\\nai-i-ve-AI\\"+dir+".csv")
+		if dir>"puppy":
+			subpath = os.path.join(path, dir)
+			myfeatures=[]
+			print(subpath)
+			cnt=0
+			if os.path.isdir(subpath):
+				for subdir in os.listdir(subpath):
+					cnt+=1
+					print(cnt)
+					subsubpath=os.path.join(subpath, subdir)
+					#load image
+					img = numpy.array(base.loadimage(subsubpath))
+					#print(subsubpath)
+					img = (skimage.color.rgb2gray(img)*255).astype(numpy.uint8)
+			        #features
+					myfeatures.append(base.feature(img))
+				load="C:\\Users\\asus-pc\\Documents\\GitHub\\nai-i-ve-AI\\"+dir+".pkl"
+
+				#writetocsv(load, myfeatures)
+				wf = open(load, 'wb')
+				pickle.dump(myfeatures, file=wf)
+				#data=pandas.DataFrame(myfeatures)
+				#data.to_csv("C:\\Users\\asus-pc\\Documents\\GitHub\\nai-i-ve-AI\\"+dir+".csv")
 
 
 numpy.set_printoptions(threshold = 1e6)
